ab1ef5952c138e99c77318a88f32e3362b28cd54
Kovalov Dima <wresser1grr.la@gmail.com>
2021-05-30 22:25:42 +0300

    Changed oauth endpoint and changed gitignore

--- petition_service/petition_service/settings.py
+++ petition_service/petition_service/settings.py
@@ -159,7 +159,7 @@ DJOSER = {
     'ACTIVATION_URL': '#activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': False,
     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
-    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:8000',],
+    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:4200/oauth',],
     'SERIALIZERS' : {
         'user_create': 'api.serializers.UserRegisterSerializer'
     }
