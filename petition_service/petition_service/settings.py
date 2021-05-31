065c2dac82ecf2a93ba91fa85c79a9b7daa867f6
Kovalov Dima <wresser1grr.la@gmail.com>
2021-05-31 18:09:19 +0300

    Changed db settings

--- petition_service/petition_service/settings.py
+++ petition_service/petition_service/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'petition_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'postgres',
-#        'USER': 'postgres',
-#        'PASSWORD': 'db_password123',
-#        'HOST': 'db',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'db_password123',
+        'HOST': 'db',
+        'PORT': '5435',
+    }
+}
 
 
 # Password validation
